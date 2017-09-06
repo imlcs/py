9bc8f24a0709996c46830b119303062badf257ba
LCS <992874270@qq.com>
2017-09-06 23:09:39 +0800

    添加Python3支持

--- SystemOperation/list.py
+++ SystemOperation/list.py
@@ -8,9 +8,14 @@
 
 #/usr/bin/python
 import os
-
-for roots,dirs,files in os.walk('/usr/src'):
-	#roots是目录路径，dirs是目录下的子目录，files是目录下的所有文件
-	#print roots
-	for file in files:
-		print os.path.join(roots,file)
+from pathlib import Path
+for roots,dirs,files in os.walk('/root'):
+    #roots是目录路径，dirs是目录下的子目录，files是目录下的所有文件
+    #print roots
+    dirpath = Path(roots)
+    for file in files:
+        '''
+            os.path.join 是Python2的写法，Python3 中取消这个方法，用 pathlib 模块的 Path 方法替代
+        '''
+        #print os.path.join(roots,file)
+        print(dirpath / file)
