bf636be81ffe7b31e6deafbe68b195c567b290b9
LCS <992874270@qq.com>
2017-09-24 21:34:35 +0800

    测试orm

---
+++ Async/webapp/www/test_orm.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+import orms, asyncio, sys
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orms.create_pool(loop=loop, user='root', host='192.168.1.11', password='123456', db='webapp')
+    u = User(name='wangwu', email='wangwu@example.com', passwd='1234567890', image='about:blank')
+    await u.save()
+    await orms.destory_pool()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
